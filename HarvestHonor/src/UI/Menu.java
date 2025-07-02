bbdd2791998c63dfe2618e8810657a36f185cd01
Fadi Alhazim <fa223fm@student.lnu.se>
2025-07-02 16:27:25 +0200

    Functional Buttons
    Added Functional buttons to start the game and close the game

--- HarvestHonor/src/UI/Menu.java
+++ HarvestHonor/src/UI/Menu.java
@@ -1,14 +1,16 @@
 package UI;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Menu {
 
     protected static JFrame menu;
     protected static JPanel menuPanel;
-    protected static JButton startGame;
-    protected static JButton closeGame;
+    protected static JButton startGameBtn;
+    protected static JButton closeGameBtn;
 
     public Menu()
     {
@@ -18,14 +20,14 @@ public class Menu {
 
     /**
      * This is a method that starts the frame of the menu
-     *
      */
     protected static void windowStarter()
     {
         menu = new JFrame("Harvest and Honor");
-        menu.setBounds(300,300, 1000,500);
+        menu.setBounds(500,500, 500,800);
         menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         menu.setLayout(null);
+        menu.setResizable(false);
         menu.setVisible(true);
         System.out.println("The menu window is created");
 
@@ -34,14 +36,49 @@ public class Menu {
     }
 
 
+    /**
+     * This method makes a panel for the menu where everything is going to be added on
+     */
     public static void setupMenuPanel()
     {
         menuPanel = new JPanel();
-        menuPanel.setBounds(0,0, 900,450);
+        menuPanel.setBounds(10,10, 465,740);
         menuPanel.setLayout(null);
-        menuPanel.setBackground(new Color(245, 245, 220));
+        menuPanel.setBackground(new Color(213, 213, 100));
         menu.add(menuPanel);
         System.out.println("Jpanel for menu created");
+
+        setupMenuButtons();
     }
 
+
+
+    public static void setupMenuButtons()
+    {
+        // setting up game start button
+        startGameBtn = new JButton("Start");
+        startGameBtn.setBounds(25,590, 100, 50);
+        startGameBtn.setVisible(true);
+        startGameBtn.setBackground(Color.RED);
+        menuPanel.add(startGameBtn);
+
+        // Setting up close button
+        closeGameBtn = new JButton("Close");
+        closeGameBtn.setBounds(335, 590, 100,50);
+        closeGameBtn.setBackground(Color.BLUE);
+        closeGameBtn.setVisible(true);
+        closeGameBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        menuPanel.add(closeGameBtn);
+
+        //Refresh the menu Panel
+        menuPanel.revalidate();
+        menuPanel.repaint();
+
+    }
 }
