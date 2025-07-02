7ba8fd9cd84ccafad12eebd61465acaf6e93f5e6
WaybleDoo <raymi.eliasson420@gmail.com>
2025-07-02 18:50:27 +0200

    Enemy class code

--- HarvestHonor/src/Main.java
+++ HarvestHonor/src/Main.java
@@ -1,6 +1,8 @@
 import UI.MainPanel;
 import UI.Window;
 import Units.Building;
+import Units.Enemy;
+import Units.EnemySoldier;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,20 +18,22 @@ public class Main
     public static Window window;
     public static MainPanel panel;
     public static Building building;
+    public static Enemy[] enemySoldiers;
 
     public static void main(String[] args)
     {
         window = new Window();
         panel = new MainPanel();
         building = new Building();
+        enemySoldiers =  new EnemySoldier[10];
 
         setupGame();
     }
 
     public static void setupGame()
     {
-        window.add(panel);
         panel.setup();
+        window.add(panel);
         window.setup();
         building.setup();
 
--- HarvestHonor/src/UI/MainPanel.java
+++ HarvestHonor/src/UI/MainPanel.java
@@ -7,9 +7,13 @@ public class MainPanel extends JPanel
 {
 
     public static Image image = new ImageIcon(MainPanel.class.getResource("/Bilder/gräs.png")).getImage();
+    //public static Image baseEnemy = new ImageIcon(MainPanel.class.getResource("/Bilder/gubbe.png")).getImage();
+    //public ImageIcon baseEnemy = new ImageIcon("/Bilder/gubbe.png");
+    //JLabel label = new JLabel(baseEnemy);
 
     public void setup()
     {
+        //this.add(label);
         this.setPreferredSize(new Dimension(500, 500));
         this.setLayout(null);
         this.setVisible(true);
